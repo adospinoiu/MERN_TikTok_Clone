11a02da41f54a68588021283012a7463c5ec10b0
adospinoiu <alex.dospinoiu@gmail.com>
2021-03-10 05:47:56 -0700

    Added first video to Video.css. Does not render to the browser

--- tiktok-clone/src/App.js
+++ tiktok-clone/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Video from './Video.js';
 
 function App() {
   return (
     <div className="app">
       <h1>Full-Stack MERN Tik-Tok App</h1>
+
+      <Video />
 
       {/* app container
         videos
