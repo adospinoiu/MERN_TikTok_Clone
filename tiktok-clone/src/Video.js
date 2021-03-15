613eb0db8ede80ce64c1ea4963fc79eaddea8f33
adospinoiu <alex.dospinoiu@gmail.com>
2021-03-15 06:17:08 -0700

    Video.jss component still not working properly; however, added class and description to component

--- tiktok-clone/src/Video.js
+++ tiktok-clone/src/Video.js
@@ -7,7 +7,10 @@ function Video() {
         <div className="video">
             <h1>Video Import</h1>
             
-            <video src="https://youtu.be/vz9n-2W33WY"></video>
+            <video
+            className="video__player"
+            loop
+            src="https://youtu.be/ktjafK4SgWM"></video>
             {/* <iframe width="1280" height="720" src="https://www.youtube.com/embed/vz9n-2W33WY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
 
             {/* VideoFooter */}
