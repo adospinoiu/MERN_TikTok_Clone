a5b2c41ff1ea69d3d7a6aa2e2bcdb53c4cf92bca
adospinoiu <alex.dospinoiu@gmail.com>
2021-03-17 05:19:31 -0700

    Created files for VidoeSidebar and VideoFooter; both .js and .css files

--- tiktok-clone/src/Video.js
+++ tiktok-clone/src/Video.js
@@ -7,11 +7,13 @@ function Video() {
     const videoRef = useRef(null);
 
     const handleVideoPress = () => {
-        // if video is playing
-        // stop it
-
-        // otherwise if its not playing
-        // play it
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
     }
 
     return (
@@ -21,7 +23,7 @@ function Video() {
             className="video__player"
             loop
             ref={videoRef}
-            src="https://v25-us.tiktokcdn.com/d61f5a0e571b59fffb16db13bc904ec3/605107cd/video/tos/useast2a/tos-useast2a-pve-0068/aff95613715a4beb806b06dfeaa91040/?a=1233&br=3800&bt=1900&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210316133136010189049225531E1E0A&lr=tiktok_m&mime_type=video_mp4&pl=0&qs=0&rc=Mzl4OmVyPHA3MzMzOjczM0ApaWY3Mzo4OGQ7Nzc0OjtlN2dfamdlc2c0YnJgLS00MTZzcy40Ml9hXmBeL19iLzEwXmE6Yw%3D%3D&vl=&vr="></video>
+            src="https://v39-us.tiktokcdn.com/febb17ecb4bdc3dad90991734636f1af/60524188/video/tos/alisg/tos-alisg-pve-0037c001/e3fc3a4a6c1c4d1da3c081928946e3d7/?a=1233&br=1680&bt=840&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210317115048010189074148100ED5E4&lr=tiktok_m&mime_type=video_mp4&pl=0&qs=0&rc=M213cTZ4cWs3NDMzZTczM0ApZDw0ZWY4OGQ5N2g8OzxkOmcuc2A1amxhNjRgLS1eMTRzc2AyYV4tMTVgXmNeLS81NWM6Yw%3D%3D&vl=&vr="></video>
 
             {/* VideoFooter */}
             {/* VideoSidebar */}
---
+++ tiktok-clone/src/VideoSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./VidoeSidebar.css";
+
+function VideoSidebar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VideoSidebar
