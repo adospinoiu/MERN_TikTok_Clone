4f1f15035a8e0e3a7fdc4b967df66221395968db
adospinoiu <alex.dospinoiu@gmail.com>
2021-03-20 06:30:16 -0700

    Added props: channel, description, song to VideoFooter.js component and made sure they get passed from Vidoe.js

--- tiktok-clone/src/Video.js
+++ tiktok-clone/src/Video.js
@@ -28,8 +28,16 @@ function Video() {
             ref={videoRef}
             src="https://v21-us.tiktokcdn.com/video/tos/useast2a/tos-useast2a-pve-0068/2b219dd107dc4e92b8340a21ab901d20/?VExpiration=1616178484&VSignature=98fa22f30309297fd5327e09855ad7be&a=1233&br=3818&bt=1909&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210319122732010189073132561048BE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M207bGk5bGtzNDMzaDczM0ApOWlkMzQ1aWVlNzU3NzlkOWdhX29yYTRmMTFgLS0wMTZzcy8uYC5hXmNhLl5iMTJfYDA6Yw%3D%3D&vl=&vr="></video>
 
-            <VideoFooter />
-            <VideoSidebar likes={111} messages={222} shares={333} />
+            <VideoFooter
+                channel="adospinoiu"
+                description="MERN Stack Clone"
+                song="Awesome Song"
+            />
+            <VideoSidebar
+                likes={111} 
+                messages={222} 
+                shares={333} 
+            />
         </div>
     )
 }
